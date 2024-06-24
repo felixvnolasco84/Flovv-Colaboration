3f393b0291501caba8a34a50bd009d29ac8ebbfc
felixvnolasco84 <felix@polygonag.com>
2024-06-24 16:17:50 -0600

    chore: Add .next folder to .gitignore and update Loading component with Spanish translation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,8 @@ import styles from "./Loading.module.css";
 export function Loading() {
   return (
     <div className={styles.loading}>
-      <img src="https://liveblocks.io/loading.svg" alt="Loading" />
+      {/* <img src="https://liveblocks.io/loading.svg" alt="Loading" /> */}
+      <p>Cargando</p>
     </div>
   );
 }
